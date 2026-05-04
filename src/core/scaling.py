7f387489355da7e3f8a1d9acaaefdd797309d19b
kali <michalex37@proton.me>
2026-05-04 15:06:20 +0200

    Mon message de commit

--- src/core/scaling.py
+++ src/core/scaling.py
@@ -1,4 +1,5 @@
 import pygame
+import constant
 from utils import log
 def auto_scaling():
     """
@@ -13,9 +14,9 @@ def auto_scaling():
     log.logger.send(str(size))
     #pygame.display.toggle_fullscreen() # fullscreen
     #w, h = pygame.display.get_surface().get_size() # utilise le fait que c'est en fullscreen pour récuperer la taille de l'écran
-    if largeur < 1000 or hauteur < 1000:
-        w2 = int(1000/100 * largeur)
-        h2 = int(1000/100 * hauteur)
+    if largeur < constant.SCREEN_WIDTH or hauteur < constant.SCREEN_HEIGHT:
+        w2 = int(SCREEN_WITDH / 2)
+        h2 = int(SCREEN_HEIGHT / 2)
         return w2, h2
     else:
         return False
