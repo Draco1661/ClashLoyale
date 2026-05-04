455730505e33928ab96dd66c507f71f7c1f023a7
kali <michalex37@proton.me>
2026-05-04 15:17:38 +0200

    scaling patch

--- src/core/scaling.py
+++ src/core/scaling.py
@@ -1,22 +1,29 @@
 import pygame
 import constant
 from utils import log
+
 def auto_scaling():
     """
-    called at startup
-    display fullscreen game and use this fullscreen to get the size of the screen
-    if the screen is too small return True, if not return False
+    Returns a scaling factor or adapted resolution based on screen size.
     """
-    size = pygame.display.get_desktop_sizes()
-    largeur = size[0][0]
-    hauteur = size[0][1]
-    log.logger.send("size of the screen")
-    log.logger.send(str(size))
-    #pygame.display.toggle_fullscreen() # fullscreen
-    #w, h = pygame.display.get_surface().get_size() # utilise le fait que c'est en fullscreen pour récuperer la taille de l'écran
-    if largeur < constant.SCREEN_WIDTH or hauteur < constant.SCREEN_HEIGHT:
-        w2 = int(SCREEN_WITDH / 2)
-        h2 = int(SCREEN_HEIGHT / 2)
-        return w2, h2
-    else:
-        return False
+
+    info = pygame.display.Info()
+    screen_w, screen_h = info.current_w, info.current_h
+
+    base_w = constant.SCREEN_WIDTH
+    base_h = constant.SCREEN_HEIGHT
+
+    log.logger.send("Size of the window")
+    log.logger.send(f"{screen_w}x{screen_h}")
+
+    scale_w = screen_w / base_w
+    scale_h = screen_h / base_h
+
+    scale = min(scale_w, scale_h)
+
+    if scale < 1:
+        new_w = int(base_w * scale)
+        new_h = int(base_h * scale)
+        return new_w, new_h
+
+    return base_w, base_h
